daebbf20379cd72b864768f99de50565a8b26477
Steve Richert <steve.richert@gmail.com>
2015-08-18 15:50:11 -0400

    Add tests around pinging a tally with custom data

--- spec/requests/tally_ping_spec.rb
+++ spec/requests/tally_ping_spec.rb
@@ -1,6 +1,6 @@
 describe "Tally Ping" do
   let!(:tally) do
-    create(:tally, pattern: "((?<user>[^ ]+) )?swore( (?<number>\d+) times)?")
+    create(:tally, pattern: '^((?<user>[^ ]+) )?swore( (?<number>\d+) times)?$')
   end
 
   it "creates a data point for the matching tally" do
@@ -8,11 +8,50 @@ describe "Tally Ping" do
       post "/slack", text: "swore", user_name: "steve"
     }.to change {
       DataPoint.count
-    }.by(1)
+    }.from(0).to(1)
 
     data_point = DataPoint.last
     expect(data_point.metric).to eq(tally)
     expect(data_point.number).to eq(1)
     expect(data_point.user).to eq("steve")
+  end
+
+  it "creates a data point with a different number" do
+    expect {
+      post "/slack", text: "swore 2 times", user_name: "steve"
+    }.to change {
+      DataPoint.count
+    }.from(0).to(1)
+
+    data_point = DataPoint.last
+    expect(data_point.metric).to eq(tally)
+    expect(data_point.number).to eq(2)
+    expect(data_point.user).to eq("steve")
+  end
+
+  it "creates a data point with a different user" do
+    expect {
+      post "/slack", text: "brian swore", user_name: "steve"
+    }.to change {
+      DataPoint.count
+    }.from(0).to(1)
+
+    data_point = DataPoint.last
+    expect(data_point.metric).to eq(tally)
+    expect(data_point.number).to eq(1)
+    expect(data_point.user).to eq("brian")
+  end
+
+  it "creates a data point with a different number and user" do
+    expect {
+      post "/slack", text: "brian swore 2 times", user_name: "steve"
+    }.to change {
+      DataPoint.count
+    }.from(0).to(1)
+
+    data_point = DataPoint.last
+    expect(data_point.metric).to eq(tally)
+    expect(data_point.number).to eq(2)
+    expect(data_point.user).to eq("brian")
   end
 end
