19c52c60aa9ef452da32bbf770fe3a225b0845c1
Steve Richert <steve.richert@gmail.com>
2015-08-18 15:52:42 -0400

    Test that a tally metric can accept a decimal number

--- spec/requests/tally_ping_spec.rb
+++ spec/requests/tally_ping_spec.rb
@@ -1,6 +1,8 @@
 describe "Tally Ping" do
   let!(:tally) do
-    create(:tally, pattern: '^((?<user>[^ ]+) )?swore( (?<number>\d+) times)?$')
+    create(:tally,
+      pattern: '^((?<user>[^ ]+) )?swore( (?<number>\d+(\.\d+)?) times)?$'
+    )
   end
 
   it "creates a data point for the matching tally" do
@@ -53,5 +55,18 @@ describe "Tally Ping" do
     expect(data_point.metric).to eq(tally)
     expect(data_point.number).to eq(2)
     expect(data_point.user).to eq("brian")
+  end
+
+  it "accepts decimal numbers" do
+    expect {
+      post "/slack", text: "swore 1.5 times", user_name: "steve"
+    }.to change {
+      DataPoint.count
+    }.from(0).to(1)
+
+    data_point = DataPoint.last
+    expect(data_point.metric).to eq(tally)
+    expect(data_point.number).to eq(1.5)
+    expect(data_point.user).to eq("steve")
   end
 end
