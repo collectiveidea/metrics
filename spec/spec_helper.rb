a2e8e44844a0e1a8d01f2f73ab5e08f76d8ff784
Steve Richert <steve.richert@gmail.com>
2015-08-18 13:06:43 -0400

    Install RSpec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,16 @@
+ENV["RAILS_ENV"] ||= "test"
+
+require File.expand_path("../../config/environment", __FILE__)
+require "rspec/rails"
+
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.infer_spec_type_from_file_location!
+end
+
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
