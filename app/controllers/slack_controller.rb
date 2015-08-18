5f60d31c69336f8e95c5841296770af3ba3ffc36
Steve Richert <steve.richert@gmail.com>
2015-08-18 16:27:52 -0400

    Look up metrics for incoming Slack Slash Commands more efficiently

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -1,14 +1,12 @@
 class SlackController < ApplicationController
   def slash_command
-    metrics = Metric.all.select { |m| m =~ params[:text] }
+    metric = Metric.match(params[:text])
+    metric.ping(text: params[:text], user: params[:user_name])
 
-    if metrics.one?
-      metrics.first.ping(text: params[:text], user: params[:user_name])
-      render text: t("ping.confirmation")
-    elsif metrics.none?
-      render text: t("ping.no_matching_metrics"), status: :conflict
-    else
-      render text: t("ping.multiple_matching_metrics"), status: :conflict
-    end
+    render text: t("ping.created"), status: :created
+  rescue Metric::NoMatch
+    render text: t("ping.no_matching_metrics"), status: :unprocessible_entity
+  rescue Metric::MultipleMatches
+    render text: t("ping.multiple_matches"), status: :conflict
   end
 end
--- app/models/metric.rb
+++ app/models/metric.rb
@@ -1,10 +1,22 @@
 class Metric < ActiveRecord::Base
+  class NoMatch < StandardError; end
+  class MultipleMatches < StandardError; end
+
   has_many :data_points, inverse_of: :metric, dependent: :delete_all
 
   validates :name, :pattern, presence: true
   validate :pattern_must_be_valid
 
   delegate :=~, to: :regexp
+
+  def self.match(text)
+    metrics = where(":text ~ regexp_replace(pattern, '\\?<\\w+>', '', 'g')", text: text).to_a
+
+    raise NoMatch if metrics.none?
+    raise MultipleMatches if metrics.many?
+
+    metrics.first
+  end
 
   def regexp
     Regexp.new(pattern)
