eeb7166ea3e81ea835bf4a1d74951e4a48a32a75
Steve Richert <steve.richert@gmail.com>
2015-08-20 06:10:19 -0400

    Protect the application (optionally) behind HTTP Basic Authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  http_basic_authenticate_with(
+    realm: ENV["BASIC_AUTH_REALM"],
+    name: ENV["BASIC_AUTH_NAME"],
+    password: ENV["BASIC_AUTH_PASSWORD"],
+    if: -> { ENV["BASIC_AUTH_NAME"] && ENV["BASIC_AUTH_PASSWORD"] }
+  )
 end
